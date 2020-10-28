e15f103d073a21d474cb15276c131bd271967e5e
Radu Denis Florian <rdenis9616@gmail.com>
2020-10-28 19:24:32 +0200

    Add initial nugget repositories and create needed projects.

---
+++ MultiShare.Backend/MultiShare.Backend.DataLayer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MultiShare.Backend.DataLayer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ MultiShare.Backend/MultiShare.Backend.Helpers/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MultiShare.Backend.Helpers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
